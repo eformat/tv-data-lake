6c7362dbfd9243ec8f7e04fe13f7bb14e4840cca
Mike Hepburn <eformat@gmail.com>
2020-08-16 18:42:53 +1000

    🦣 add other data points 🦣

--- src/main/java/org/acme/s3/S3SyncClientResource.java
+++ src/main/java/org/acme/s3/S3SyncClientResource.java
@@ -12,13 +12,7 @@ import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class S3SyncClientResource {
@@ -28,13 +22,22 @@ public class S3SyncClientResource {
     @Inject
     S3Client s3;
 
-    @ConfigProperty(name = "bucket.name", defaultValue = "default-bucket")
-    String bucketName;
+    @ConfigProperty(name = "r1.bucket.name")
+    String r1bucketName;
+
+    @ConfigProperty(name = "r5.bucket.name")
+    String r5bucketName;
+
+    @ConfigProperty(name = "t1.bucket.name")
+    String t1bucketName;
+
+    @ConfigProperty(name = "t5.bucket.name")
+    String t5bucketName;
 
     @Incoming("tv-in-r1")
     public void storeInS3R1(JsonObject aggregation) {
-        String cacheKey = String.format("%s-%s-%s", "r1", aggregation.getString("key"), Instant.now());
-        PutObjectResponse putResponse = s3.putObject(buildPutRequest(cacheKey, aggregation), RequestBody.fromString(aggregation.toString()));
+        String cacheKey = String.format("%s-%s-%s", "r1", aggregation.getString("key"), aggregation.getString("instant"));
+        PutObjectResponse putResponse = s3.putObject(buildPutRequest(r1bucketName, cacheKey, aggregation), RequestBody.fromString(aggregation.toString()));
         if (putResponse != null) {
             // ok
             log.debug("::storeInS3R1 ok: " + putResponse);
@@ -43,7 +46,43 @@ public class S3SyncClientResource {
         }
     }
 
-    protected PutObjectRequest buildPutRequest(String key, JsonObject data) {
+    @Incoming("tv-in-r5")
+    public void storeInS3R5(JsonObject aggregation) {
+        String cacheKey = String.format("%s-%s-%s", "r5", aggregation.getString("key"), aggregation.getString("instant"));
+        PutObjectResponse putResponse = s3.putObject(buildPutRequest(r1bucketName, cacheKey, aggregation), RequestBody.fromString(aggregation.toString()));
+        if (putResponse != null) {
+            // ok
+            log.debug("::storeInS3R5 ok: " + putResponse);
+        } else {
+            log.warn("::storeInS3R5 error: " + aggregation);
+        }
+    }
+
+    @Incoming("tv-in-t1")
+    public void storeInS3T1(JsonObject aggregation) {
+        String cacheKey = String.format("%s-%s-%s", "t1", aggregation.getString("key"), aggregation.getString("instant"));
+        PutObjectResponse putResponse = s3.putObject(buildPutRequest(r1bucketName, cacheKey, aggregation), RequestBody.fromString(aggregation.toString()));
+        if (putResponse != null) {
+            // ok
+            log.debug("::storeInS3T1 ok: " + putResponse);
+        } else {
+            log.warn("::storeInS3T1 error: " + aggregation);
+        }
+    }
+
+    @Incoming("tv-in-t5")
+    public void storeInS3T5(JsonObject aggregation) {
+        String cacheKey = String.format("%s-%s-%s", "t5", aggregation.getString("key"), aggregation.getString("instant"));
+        PutObjectResponse putResponse = s3.putObject(buildPutRequest(r1bucketName, cacheKey, aggregation), RequestBody.fromString(aggregation.toString()));
+        if (putResponse != null) {
+            // ok
+            log.debug("::storeInS3T5 ok: " + putResponse);
+        } else {
+            log.warn("::storeInS3T5 error: " + aggregation);
+        }
+    }
+
+    protected PutObjectRequest buildPutRequest(String bucketName, String key, JsonObject data) {
         return PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
