55f275160c9b59a092b10d1c0d77ea6f36076f14
Mike Hepburn <eformat@gmail.com>
2020-08-30 15:55:52 +1000

    🪱 remove test stub 🪱

--- src/test/java/org/acme/s3/NativeS3SyncClientResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.s3;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeS3SyncClientResourceIT extends S3SyncClientResourceTest {
-
-    // Execute the same tests but in native mode.
-}
